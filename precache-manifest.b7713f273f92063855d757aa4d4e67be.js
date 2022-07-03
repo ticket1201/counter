0e7215bc06238c2b91703bf16ffef15ee07e94f2
Anton Metlitsky <ticket1201@gmail.com>
2022-07-03 17:55:49 +0300

    Updates

---
+++ precache-manifest.b7713f273f92063855d757aa4d4e67be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85119ba21c38c5ab17f5",
+    "url": "/counter/static/css/main.051d60ac.chunk.css"
+  },
+  {
+    "revision": "85119ba21c38c5ab17f5",
+    "url": "/counter/static/js/main.30c17b7b.chunk.js"
+  },
+  {
+    "revision": "395f42efa7c50e4ddc67",
+    "url": "/counter/static/js/runtime~main.16ddd954.js"
+  },
+  {
+    "revision": "ee6531dacd6bd9bb81f7",
+    "url": "/counter/static/js/2.f862b053.chunk.js"
+  },
+  {
+    "revision": "b48344158fdfbb9d14c769612cbc048f",
+    "url": "/counter/index.html"
+  }
+];
